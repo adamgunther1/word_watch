9d9c632347a9d2746f18fb8f8d74be271e4dd97d
Adam Gunther <adamgunther1@gmail.com>
2017-09-05 11:09:45 -0600

    refactors to classes and es6 syntax

--- src/topWord.js
+++ src/topWord.js
@@ -1,30 +1,33 @@
 const $ = require('jquery')
 const API =  'https://wordwatch-api.herokuapp.com'
 
-function TopWord (word) {
-  this.name = word.name
-  this.count = word.count
-}
+class TopWord {
+  
+  constructor (word) {
+    this.name = word.name
+    this.count = word.count
+  }
 
-TopWord.prototype.toHTML = function () {
-  return `${this.name} (${this.count})`
-}
+  toHTML () {
+    return `${this.name} (${this.count})`
+  }
 
-TopWord.findAndCountTopWord = function () {
-  return this.getTopWord ()
-  .then( function (word) {
-    let name = Object.keys(word.word)[0]
-    let count = Object.values(word.word)[0]
-    let wordObj = { name, count}
-    return new TopWord (wordObj)
-  })
-  .then(function (topWord) {
-    return topWord.toHTML ()
-  })
-}
+  static findAndCountTopWord () {
+    return this.getTopWord ()
+    .then( function (word) {
+      let name = Object.keys(word.word)[0]
+      let count = Object.values(word.word)[0]
+      let wordObj = { name, count}
+      return new TopWord (wordObj)
+    })
+    .then(function (topWord) {
+      return topWord.toHTML ()
+    })
+  }
 
-TopWord.getTopWord = function () {
-  return $.getJSON(`${API}/api/v1/top_word`)
+  static getTopWord () {
+    return $.getJSON(`${API}/api/v1/top_word`)
+  }
 }
 
 module.exports = TopWord
--- src/wordCloud.js
+++ src/wordCloud.js
@@ -1,60 +1,63 @@
 const $ = require('jquery')
 const API =  'https://wordwatch-api.herokuapp.com'
 
-function WordCloud (word) {
-  this.name = word.name
-  this.count = word.count
-}
+class WordCloud {
+  
+  constructor (word) {
+    this.name = word.name
+    this.count = word.count
+  }
 
-WordCloud.prototype.toHTML = function () {
-  return `<p style="font-size: ${this.count}em;">${this.name}</p>`
-}
+  toHTML () {
+    return `<p style="font-size: ${this.count}em;">${this.name}</p>`
+  }
 
-WordCloud.appendWords = function () {
-  let paragraph = this.previousElementSibling.value
-  let wordCollection = paragraph.split(' ')
-  let downcasedWords = WordCloud.downcase(wordCollection)
-  WordCloud.postWords (downcasedWords)
-  let wordsObj = WordCloud.countWords(downcasedWords)
-  let wordCloudObjects = Object.keys(wordsObj).map(function(key) {
-    let word = {name: key, count: wordsObj[key]}
-    return new WordCloud (word)
-  })
-  $( ".word-count p" ).remove()
-  return wordCloudObjects.forEach(function (wordCloudObject) {
-    $('.word-count').append(wordCloudObject.toHTML ())
-  })
-}
-
-WordCloud.postWords = function (words) {
-  words.forEach(function (word) {
-    $.ajax ({
-      type: 'POST',
-      url: `${API}/api/v1/words`,
-      data: { word: { value: word }}
+  static appendWords () {
+    let paragraph = this.previousElementSibling.value
+    let wordCollection = paragraph.split(' ')
+    let downcasedWords = WordCloud.downcase(wordCollection)
+    WordCloud.postWords (downcasedWords)
+    let wordsObj = WordCloud.countWords(downcasedWords)
+    let wordCloudObjects = Object.keys(wordsObj).map(function(key) {
+      let word = {name: key, count: wordsObj[key]}
+      return new WordCloud (word)
     })
-    .then(function () {
-      console.log(`${word} posted successfully`)
+    $( ".word-count p" ).remove()
+    return wordCloudObjects.forEach(function (wordCloudObject) {
+      $('.word-count').append(wordCloudObject.toHTML ())
     })
-  })
-}
+  }
 
-WordCloud.downcase = function (words) {
-  return words.map(function (word) {
-    return word.toLowerCase()
-  })
-}
+  static postWords (words) {
+    words.forEach(function (word) {
+      $.ajax ({
+        type: 'POST',
+        url: `${API}/api/v1/words`,
+        data: { word: { value: word }}
+      })
+      .then(function () {
+        console.log(`${word} posted successfully`)
+      })
+    })
+  }
 
-WordCloud.countWords = function (words) {
-  let wordsObj = {}
-  words.forEach(function (word) {
-    if (wordsObj[word]) {
-      wordsObj[word] += 1
-    } else {
-      wordsObj[word] = 1
-    }
-  })
-  return wordsObj
+  static downcase (words) {
+    return words.map(function (word) {
+      return word.toLowerCase()
+    })
+  }
+
+  static countWords (words) {
+    let wordsObj = {}
+    words.forEach(function (word) {
+      if (wordsObj[word]) {
+        wordsObj[word] += 1
+      } else {
+        wordsObj[word] = 1
+      }
+    })
+    return wordsObj
+  }
 }
 
 
